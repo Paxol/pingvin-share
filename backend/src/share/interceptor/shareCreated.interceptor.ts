6c2382ab364a088bb64a89bf1c3408a900ab8b0c
Mirko Passoli <mirko.passoli@gmail.com>
2023-05-29 08:25:45 +0200

    Added share url to share-created webhook data

--- backend/src/share/interceptor/shareCreated.interceptor.ts
+++ backend/src/share/interceptor/shareCreated.interceptor.ts
@@ -5,6 +5,7 @@ import {
   NestInterceptor,
 } from "@nestjs/common";
 import { Observable, tap } from "rxjs";
+import { ConfigService } from "src/config/config.service";
 import { PrismaService } from "src/prisma/prisma.service";
 import { FiredWebhookDTO } from "src/webhook/dto/FiredWebhookDTO";
 import { WebhookService } from "src/webhook/webhook.service";
@@ -13,20 +14,25 @@ import { WebhookService } from "src/webhook/webhook.service";
 export class ShareCreatedInterceptor implements NestInterceptor {
   constructor(
     private readonly prisma: PrismaService,
-    private readonly webhookService: WebhookService
+    private readonly webhookService: WebhookService,
+    private readonly config: ConfigService
   ) {}
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       tap(async (x) => {
-        const data = await this.prisma.share.findFirst({
+        const share = await this.prisma.share.findFirst({
           where: { id: x.id },
         });
 
-        this.webhookService.fire(new FiredWebhookDTO().from({
-          event: "share-created",
-          data,
-        }));
+        const url = `${this.config.get("general.appUrl")}/share/${share.id}`;
+
+        this.webhookService.fire(
+          new FiredWebhookDTO().from({
+            event: "share-created",
+            data: { ...share, url },
+          })
+        );
       })
     );
   }
